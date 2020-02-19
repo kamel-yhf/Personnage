054d5e06cf4ed4ca5908db2449b9869d00bbdeed
kamel-yhf <kamelyakhlef@hotmail.fr>
2020-02-19 09:42:07 +0100

    getter personnage

---
+++ Personnage.php
@@ -0,0 +1,42 @@
+﻿<?php
+
+class Personnage{
+
+    private $nom;
+    private $force;
+    private $sante;
+
+//GETTER
+
+/**
+ * return $nom
+ * type: string
+ */
+    public function getNom(){
+        return $this->nom;
+    }
+
+/**
+ * return $force
+ * type: integer
+ */
+public function getForce(){
+    return $this->force;
+}
+
+/**
+ * return $sante
+ * type: integer
+ */
+public function getSante(){
+    return $this->sante;
+}
+
+
+    public function seDeplacer(){
+
+    }
+    public function parler(){
+
+    }
+}
